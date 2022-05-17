b3fd2273bcedac91823d7a8c58a4e6da351919dc
Ahmed-ITCS <59608481+Ahmed-ITCS@users.noreply.github.com>
2022-05-17 18:54:39 +0500

    Add files via upload

---
+++ run.js
@@ -0,0 +1,46 @@
+window.count = 0;
+function add()
+{
+	window.location.href = "add.html";
+	count++;
+}
+window.studentnames = [];
+window.studentregs = [];
+function addd()
+{
+	names = document.getElementById("names").value;
+	regs = document.getElementById("reg").value;
+	studentnames.push(names);
+	studentregs.push(regs);
+	console.log(studentnames);
+	console.log(studentregs);
+	window.location.href = "menu.HTML";
+	count=count+1;
+}
+function deletee()
+{
+	window.location.href = "delete.html";
+}
+function deleteee()
+{
+	names = document.getElementById("namess").value;
+	regs = document.getElementById("regg").value;
+	studentnames.pop(names);
+	studentregs.pop(regs);
+	window.location.href = "menu.HTML";
+	count=count-1;
+}
+console.log(count)
+function display()
+{
+	console.log(studentnames);
+	console.log(studentregs);
+	//window.location.href = "display.html";	
+	/*for (var i = 0; i < count ;i++) {
+		console.log(i)
+	}*/
+}
+function MENU()
+{
+	window.location.href = "menu.HTML";	
+}
